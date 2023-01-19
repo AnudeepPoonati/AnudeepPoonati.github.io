dfbed65a541d0157dfef7788034637ebe47d93c3
AnudeepPoonati <105741033+AnudeepPoonati@users.noreply.github.com>
2023-01-19 15:20:33 -0800

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,30 +1,3 @@
-/* ================Toggle style switcher ===============================*/
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
-    document.querySelector(".style-switcher").classList.toggle("open");
-})
-//====================hide style - switcher on scroll===============
-window.addEventListener("scroll", () => {
-    if(document.querySelector(".style-switcher").classList.contains("open"))
-    {
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
-})
-/* ================Swicth theme colors ===============================*/
-const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color)
-{
-    alternateStyles.forEach((style) => {
-        if(color === style.getAttribute("title"))
-        {
-            style.removeAttribute("disabled");
-        }
-        else
-        {
-            style.setAttribute("disabled","true");
-        }
-    })
-}
 /* ================Day and Night Theme ===============================*/
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
